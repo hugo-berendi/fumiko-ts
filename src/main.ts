48c4b2511c0908f6f9582eb9adeeb35f5b3deace
Kamachi <hugo.berendi@outlook.de>
2022-11-19 19:18:04 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import * as eris from "eris"
-import * as dotenv from "dotenv"
+import * as eris from "eris";
+import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const token: string = process.env.DISCORD_TOKEN as string
+const token: string = process.env.DISCORD_TOKEN as string;
 
-const bot = new eris.Client(token)
+const bot = new eris.Client(token);
+
+bot.on("ready", () => {
+  console.log(`Connected as ${bot.user.username}#${bot.user.discriminator}`);
+})
+
+bot.connect()
