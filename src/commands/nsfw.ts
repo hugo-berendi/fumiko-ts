b84730229526326c760e2b6b19463f82ee0f006c
Kamachi <hugo.berendi@outlook.de>
2022-11-22 19:58:45 +0100

    .

--- src/commands/nsfw.ts
+++ src/commands/nsfw.ts
@@ -176,7 +176,7 @@ function chooseHentai(type: string) {
 function commandCallback(interaction: hym.Interaction) {
 	if (interaction.isApplicationCommand()) {
 		if (interaction.subCommand == 'hentai') {
-			const tag = interaction.options[0].name
+			const tag = interaction.options[0].value
 			const emb = new hym.Embed({
 				title: 'Hentai',
 				description: `Type: ${tag}`,
