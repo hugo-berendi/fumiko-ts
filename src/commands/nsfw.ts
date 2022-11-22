22a61d07bfca6c4fe5b32eeeee2dcd50454eb6dd
Kamachi <hugo.berendi@outlook.de>
2022-11-22 15:09:38 +0100

    .

--- src/commands/nsfw.ts
+++ src/commands/nsfw.ts
@@ -2,8 +2,6 @@ import * as hym from '@harmonyland/harmony'
 import * as cmdb from '../modules/commandBuilder'
 import akaneko from 'akaneko'
 
-const hentai = akaneko.nsfw
-
 const commandData = {
 	name: 'nsfw',
 	description: 'Sends nsfw images',
@@ -63,7 +61,59 @@ const commandData = {
 						{
 							name: 'maid',
 							value: 'maid'
-						}
+						},
+						{
+							name: 'zettaiRyouiki',
+							value: 'zettaiRyouiki'
+						},
+						{
+							name: 'yuri',
+							value: 'yuri'
+						},
+						{
+							name: 'uniform',
+							value: 'uniform'
+						},
+						{
+							name: 'uglyBastard',
+							value: 'uglyBastard'
+						},
+						{
+							name: 'thighs',
+							value: 'thighs'
+						},
+						{
+							name: 'tentacles',
+							value: 'tentacles'
+						},
+						{
+							name: 'succubus',
+							value: 'succubus'
+						},
+						{
+							name: 'school',
+							value: 'school'
+						},
+						{
+							name: 'pussy',
+							value: 'pussy'
+						},
+						{
+							name: 'panties',
+							value: 'panties'
+						},
+						{
+							name: 'orgy',
+							value: 'orgy'
+						},
+						{
+							name: 'netorare',
+							value: 'netorare'
+						},
+						{
+							name: 'masturbation',
+							value: 'masturbation'
+						},
 					]
 				}
 			] as hym.SlashCommandOption[]
@@ -72,7 +122,57 @@ const commandData = {
 	choices: [] as any[]
 }
 
-hentai.hentai()
+function chooseHentai(type: string) {
+	if (type == 'ass') {
+		return akaneko.nsfw.ass()
+	} else if (type == 'bdsm') {
+		return akaneko.nsfw.bdsm()
+	} else if (type == 'blowjob') {
+		return akaneko.nsfw.blowjob()
+	} else if (type == 'cum') {
+		return akaneko.nsfw.cum()
+	} else if (type == 'doujin') {
+		return akaneko.nsfw.doujin()
+	} else if (type == 'feet') {
+		return akaneko.nsfw.feet()
+	} else if (type == 'femdom') {
+		return akaneko.nsfw.femdom()
+	} else if (type == 'foxgirl') {
+		return akaneko.nsfw.foxgirl()
+	} else if (type == 'gifs') {
+		return akaneko.nsfw.gifs()
+	} else if (type == 'glasses') {
+		return akaneko.nsfw.glasses()
+	} else if (type == 'hentai') {
+		return akaneko.nsfw.hentai()
+	} else if (type == 'masturbation') {
+		return akaneko.nsfw.masturbation()
+	} else if (type == 'netorare') {
+		return akaneko.nsfw.netorare()
+	} else if (type == 'orgy') {
+		return akaneko.nsfw.orgy()
+	} else if (type == 'panties') {
+		return akaneko.nsfw.panties()
+	} else if (type == 'pussy') {
+		return akaneko.nsfw.pussy()
+	} else if (type == 'school') {
+		return akaneko.nsfw.school()
+	} else if (type == 'succubus') {
+		return akaneko.nsfw.succubus()
+	} else if (type == 'tentacles') {
+		return akaneko.nsfw.tentacles()
+	} else if (type == 'thighs') {
+		return akaneko.nsfw.thighs()
+	} else if (type == 'uglyBastard') {
+		return akaneko.nsfw.uglyBastard()
+	} else if (type == 'uniform') {
+		return akaneko.nsfw.uniform()
+	} else if (type == 'yuri') {
+		return akaneko.nsfw.yuri()
+	} else if (type == 'zettaiRyouiki') {
+		return akaneko.nsfw.zettaiRyouiki()
+	}
+}
 
 function commandCallback(interaction: hym.Interaction) {
 	if (interaction.isApplicationCommand()) {
@@ -81,7 +181,7 @@ function commandCallback(interaction: hym.Interaction) {
 				title: 'Hentai',
 				description: `Type: ${interaction.options[0].name}`,
 				image: {
-					url: eval(`hentai.${interaction.options[0].value}()`)
+					url: chooseHentai(interaction.options[0].value)
 				}
 			})
 
